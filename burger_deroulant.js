0d07a1669c32d4b9e900a27618c2143257c8c3e7
Hugo2202 <hugo.f@live.fr>
2020-10-26 09:21:46 +0100

    premier_commit_cv

---
+++ burger_deroulant.js
@@ -0,0 +1,25 @@
+function afficheMenu(obj){
+
+    var idBurgerDeroulant    = obj.id;
+    var idSousMenu = 'sous' + idBurgerDeroulant;
+    var sousMenu  = document.getElementById(idSousMenu);
+
+    /*****************************************************/
+    /**   on cache tous les sous-menus pour n'afficher    **/
+    /** que celui dont le menu correspondant est cliqué **/
+    /** où 4 correspond au nombre de sous-menus        **/
+    /*****************************************************/
+
+
+    if(sousMenu){
+        //alert(sousMenu.style.display);
+        if(sousMenu.style.display == "block"){
+            sousMenu.style.display = "none";
+        }
+        else{
+            sousMenu.style.display = "block";
+        }
+    }
+}
+
+
